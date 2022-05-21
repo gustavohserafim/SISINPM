3f6957762fb386e71f38a31ef47149e5e6b435c7
Gustavo Serafim <tioogu@gmail.com>
2022-05-21 16:00:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return jsonify({"para": "de crashar"})
 
 
 if __name__ == '__main__':
